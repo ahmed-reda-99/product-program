755e5262c3c5f3ea17a9cb690b65acc468398342
ahmed-reda-99 <ahmed.avy@gmail.com>
2019-09-23 15:06:01 +0200

    crearted product-program

---
+++ js/main.js
@@ -0,0 +1,55 @@
+
+var nameInp = document.getElementById("productName");
+var priceInp = document.getElementById("productPrice");
+var companyInp = document.getElementById("productCompany");
+var descInp = document.getElementById("productDesc");
+var myBtn = document.getElementById("addBtn");
+var productContainer ;
+
+
+function addProuduct(){
+    var product = {
+        name: nameInp.value,
+        price: priceInp.value,
+        company: companyInp.value,
+        desc: descInp.value,
+    }
+    productContainer.push(product);
+   
+    localStorage.setItem("productContainer",JSON.stringify(productContainer));
+
+}
+if (localStorage.getItem("productContainer")==null){
+    productContainer = [];
+}
+else{
+    productContainer =JSON.parse(localStorage.getItem("productContainer"));
+        displayData();
+
+}
+myBtn.onclick = function () {
+    addProuduct();
+    displayData();
+
+}
+
+function displayData(){
+    var cols = "";
+    for (var i =0; i<productContainer.length; i++){
+        cols+='<div class="col-md-3"><div class="product"><h3>'+productContainer[i].name+'</h3><p>'+productContainer[i].price+'</p><p>'+productContainer[i].company+'</p><p>'+productContainer[i].desc+'</p><button class=" btn btn-danger" onclick="deletProduct('+i+')">delete</buuton> </div></div>'
+    }
+    document.getElementById("rowData").innerHTML = cols;
+    clearForm();
+}
+function clearForm(){
+    var inputs = document.getElementsByClassName("form-control");
+    for (i=0;i<inputs.length;i++){
+        inputs[i].value ="";
+    }
+}
+function deletProduct(id){
+    productContainer.splice(id,1);
+    displayData();
+    localStorage.setItem("productContainer",JSON.stringify(productContainer));
+
+}
